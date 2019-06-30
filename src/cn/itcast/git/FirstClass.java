f5c4412fd2e0c846fa9bffc8aba02f5ed44a81fb
dxj <1984847121@qq.com>
2019-06-30 19:10:39 +0800

    提交项目

---
+++ src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+        System.out.println("傻屌!!!!!!!!!!!!!");
+    }
+}
